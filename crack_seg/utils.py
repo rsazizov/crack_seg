ce7a561403e782f649d6c3350f92526e2162aa2e
rsazizov <rsazizov@gmail.com>
2022-05-18 14:06:42 +0400

    Use memory efficient dice loss

--- crack_seg/utils.py
+++ crack_seg/utils.py
@@ -14,40 +14,15 @@ import albumentations as A
 from albumentations.pytorch import ToTensorV2
 
 
-# From: https://github.com/kevinzakka/pytorch-goodies/blob/master/losses.py
-def dice_loss(true, logits, eps=1e-7):
-    """Computes the Sørensen–Dice loss.
-  Note that PyTorch optimizers minimize a loss. In this
-  case, we would like to maximize the dice loss so we
-  return the negated dice loss.
-  Args:
-      true: a tensor of shape [B, 1, H, W].
-      logits: a tensor of shape [B, C, H, W]. Corresponds to
-          the raw output or logits of the model.
-      eps: added to the denominator for numerical stability.
-  Returns:
-      dice_loss: the Sørensen–Dice loss.
-  """
-    num_classes = logits.shape[1]
-    if num_classes == 1:
-        true_1_hot = th.eye(num_classes + 1)[true.squeeze(1)]
-        true_1_hot = true_1_hot.permute(0, 3, 1, 2).float()
-        true_1_hot_f = true_1_hot[:, 0:1, :, :]
-        true_1_hot_s = true_1_hot[:, 1:2, :, :]
-        true_1_hot = th.cat([true_1_hot_s, true_1_hot_f], dim=1)
-        pos_prob = th.sigmoid(logits)
-        neg_prob = 1 - pos_prob
-        probas = th.cat([pos_prob, neg_prob], dim=1)
-    else:
-        true_1_hot = th.eye(num_classes)[true.squeeze(1)]
-        true_1_hot = true_1_hot.permute(0, 3, 1, 2).float()
-        probas = th.softmax(logits, dim=1)
-    true_1_hot = true_1_hot.type(logits.type())
-    dims = (0,) + tuple(range(2, true.ndimension()))
-    intersection = th.sum(probas * true_1_hot, dims)
-    cardinality = th.sum(probas + true_1_hot, dims)
-    dice_loss = (2. * intersection / (cardinality + eps)).mean()
-    return (1 - dice_loss)
+def dice_loss(input, target):
+    smooth = 1.
+
+    iflat = input.view(-1)
+    tflat = target.view(-1)
+    intersection = (iflat * tflat).sum()
+
+    return 1 - ((2. * intersection + smooth) /
+                (iflat.sum() + tflat.sum() + smooth))
 
 
 class NormalizeInverse(T.Normalize):
